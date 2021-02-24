72a848d2a3a1c0e28fbee4205b73c5d57b7a6755
JP Eiler <jp.eiler97@gmail.com>
2021-02-24 16:45:33 -0600

    finished generatePassword function, added null checks to prevent errors

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
+//Arrays to define all characters for each character type
+//Note: please excuse the formatting, my VSCode formatter automatically breaks lines in most arrays as soon as I save
 var specialChar = [
 	'!',
 	'"',
@@ -96,123 +98,184 @@ var upperCaseChar = [
 	'Z'
 ];
 
+//getPasswordOptions prompts the user first with their password length and then which character types to include using "yes" and "no".
+
 function getPasswordOptions() {
 	var passLength = prompt('Please input the desired length of your password. Length may be between 8 and 128');
 
-	if (isNaN(passLength) === true || passLength.length === 0) {
+	//Checks password length entry
+	if (passLength === null || isNaN(passLength) === true || passLength.length === 0) {
 		alert('Please enter a number value');
 		return;
-	}
-
-	if (passLength < 8) {
+	} else if (passLength < 8) {
 		alert('Password length is too short.');
 		return;
-	}
-
-	if (passLength > 128) {
+	} else if (passLength > 128) {
 		alert('Password length is too long.');
 		return;
-	}
-
-	var specialCharCheck = prompt('Would you like to include special characters? Type yes or no.');
-
-	if (specialCharCheck.toLowerCase() === 'yes') {
-		specialCharCheck = true;
-	} else if (specialCharCheck.toLowerCase() === 'no') {
-		specialCharCheck = false;
 	} else {
-		alert('You did not enter yes or no. Your password will by default have no special characters.');
-		specialCharCheck = false;
+		//Prompts the user for which character types they would like, "yes" and "no" are the only options.
+		var specialCharCheck = prompt('Would you like to include special characters? Type yes or no.');
+
+		//Checking for null to prevent errors
+		if (specialCharCheck === null) {
+			alert('You did not enter yes or no.');
+			return;
+		} else if (specialCharCheck.toLowerCase() === 'yes') {
+			// checks for yes, .toLowerCase is used in case user inputs "YES" or "Yes"
+			specialCharCheck = true;
+		} else if (specialCharCheck.toLowerCase() === 'no') {
+			// checks for no, .toLowerCase is used in case user inputs "NO" or "No"
+			specialCharCheck = false;
+		} else {
+			alert('You did not enter yes or no.'); //all other options will stop the prompt
+			return;
+		}
+
+		//Same process is repeated for other character types
+		var numCharCheck = prompt('Would you like to include numerical characters? Type yes or no.');
+		if (numCharCheck === null) {
+			alert('You did not enter yes or no.');
+			return;
+		} else if (numCharCheck.toLowerCase() === 'yes') {
+			numCharCheck = true;
+		} else if (numCharCheck.toLowerCase() === 'no') {
+			numCharCheck = false;
+		} else {
+			alert('You did not enter yes or no.');
+			return;
+		}
+
+		var lowerCharCheck = prompt('Would you like to include lower-case characters? Type yes or no.');
+
+		if (lowerCharCheck === null) {
+			alert('You did not enter yes or no.');
+			return;
+		} else if (lowerCharCheck.toLowerCase() === 'yes') {
+			lowerCharCheck = true;
+		} else if (lowerCharCheck.toLowerCase() === 'no') {
+			lowerCharCheck = false;
+		} else {
+			alert('You did not enter yes or no.');
+			return;
+		}
+
+		var upperCharCheck = prompt('Would you like to include upper-case characters? Type yes or no.');
+		if (upperCharCheck === null) {
+			alert('You did not enter yes or no.');
+			return;
+		} else if (upperCharCheck.toLowerCase() === 'yes') {
+			upperCharCheck = true;
+		} else if (upperCharCheck.toLowerCase() === 'no') {
+			upperCharCheck = false;
+		} else {
+			alert('You did not enter yes or no.');
+			return;
+		}
+
+		if (
+			specialCharCheck === false &&
+			numCharCheck === false &&
+			lowerCharCheck === false &&
+			upperCharCheck === false
+		) {
+			alert('You must select at least one character type.');
+			return;
+		} else {
+			//Object to store character checkss to use in the password generation function
+			var characterTypes = {
+				passLength,
+				specialCharCheck,
+				numCharCheck,
+				lowerCharCheck,
+				upperCharCheck
+			};
+			return characterTypes;
+		}
 	}
-
-	var numCharCheck = prompt('Would you like to include numerical characters? Type yes or no.');
-	if (numCharCheck.toLowerCase() === 'yes') {
-		numCharCheck = true;
-	} else if (numCharCheck.toLowerCase() === 'no') {
-		numCharCheck = false;
-	} else {
-		alert('You did not enter yes or no. Your password will by default have no numerical characters.');
-		numCharCheck = false;
-	}
-
-	var lowerCharCheck = prompt('Would you like to include lower-case characters? Type yes or no.');
-	if (lowerCharCheck.toLowerCase() === 'yes') {
-		lowerCharCheck = true;
-	} else if (lowerCharCheck.toLowerCase() === 'no') {
-		lowerCharCheck = false;
-	} else {
-		alert('You did not enter yes or no. Your password will by default have no lower-case characters.');
-		lowerCharCheck = false;
-	}
-
-	var upperCharCheck = prompt('Would you like to include upper-case characters? Type yes or no.');
-	if (upperCharCheck.toLowerCase() === 'yes') {
-		upperCharCheck = true;
-	} else if (upperCharCheck.toLowerCase() === 'no') {
-		upperCharCheck = false;
-	} else {
-		alert('You did not enter yes or no. Your password will by default have no upper-case characters.');
-		upperCharCheck = false;
-	}
-
-	if (specialCharCheck === false && numCharCheck === false && lowerCharCheck === false && upperCharCheck === false) {
-		alert('You must select at least one character type.');
-		return;
-	}
-
-	var characterTypes = {
-		passLength,
-		specialCharCheck,
-		numCharCheck,
-		lowerCharCheck,
-		upperCharCheck
-	};
-
-	return characterTypes;
 }
 
+//Gets a random element from an array, defined as a function to simplify code.
 function getRandomElement(arr) {
-	var randIndex = Math.float(Math.random * arr.length);
+	var randIndex = Math.floor(Math.random() * arr.length);
 	var randElement = arr[randIndex];
 	return randElement;
 }
 
+//Function to generate password
 function generatePassword() {
+	//var options equals the object characterTypes returned by getPasswordOptions()
 	var options = getPasswordOptions();
+
+	//Prevents errors from occuring if user cancels or does not define length/types in getPasswordOptions
+	if (options === undefined || options === null) {
+		return;
+	}
+	//Array to store generated password to be returned
 	var result = [];
 
-	var possibleCharacters = [ specialChar, numChar, lowerCaseChar, upperCaseChar ];
+	//Array with all characters from the arrays of character types chosen by the user, used to generate password
+	var possibleCharacters = [];
 
+	//One character from every type chosen by user that is spliced into random index of the password to ensure every character type the user
+	//desires is put into the password string
 	var guaranteedCharacters = [];
 
+	//Checks if user wants special character type, if character check is true, the specialChar array is pushed into the possibleCharacters array,
+	//and a special character is pushed into the guaranteedCharacters array
 	if (options.specialCharCheck) {
+		possibleCharacters.push(specialChar);
 		guaranteedCharacters.push(getRandomElement(specialChar));
 	}
 
+	//Same process repeated for other character types
 	if (options.numCharCheck) {
+		possibleCharacters.push(numChar);
 		guaranteedCharacters.push(getRandomElement(numChar));
 	}
 
 	if (options.lowerCharCheck) {
+		possibleCharacters.push(lowerCaseChar);
 		guaranteedCharacters.push(getRandomElement(lowerCaseChar));
 	}
 
 	if (options.upperCharCheck) {
+		possibleCharacters.push(upperCaseChar);
 		guaranteedCharacters.push(getRandomElement(upperCaseChar));
 	}
+	//
 
-	for (var i = 0; i < options.length; i++) {
-		var possibleCharacter = getRandomElement(possibleCharacters);
+	//Random characters from the possibleCharacters are pushed into var result, based on the length of the password given by the user
+	for (i = 0; i < options.passLength; i++) {
+		var possibleCharacter = getRandomElement(getRandomElement(possibleCharacters));
+		result.push(possibleCharacter);
 	}
+
+	//Each guaranteed character is spliced into a random index in result
+	for (i = 0; i < guaranteedCharacters.length; i++) {
+		result.splice(Math.random() * result.length, 1, guaranteedCharacters[i]);
+	}
+
+	//returns the result of the password generation as a string.
+	return result.join('').toString();
 }
 
 // Write password to the #password input
 function writePassword() {
+	//sets password as string returned from generatePassword();
 	var password = generatePassword();
+
+	//Prevents value of passwordText from being set to "undefined" if password is undefined or null.
+	if (password === undefined) {
+		return;
+	}
+
+	//points to password id in HTML document
 	var passwordText = document.querySelector('#password');
 
+	//sets value of the password text in HTML to password defined by generatePassword()
 	passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', getPasswordOptions);
+generateBtn.addEventListener('click', writePassword);
